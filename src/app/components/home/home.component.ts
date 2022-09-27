abdc6445d3554bcf5c58b768c11132c21c7e3af0
Ivan Petrovic <61566864+petradev79@users.noreply.github.com>
2022-09-27 12:06:22 +0200

    implementing sort filters, create openGameDetails method when click on game (#4)
    Co-authored-by: Ivan Petrovic <ivan.petrovic@ingsoftware.com>

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
-import { APIResponse, Game } from 'src/app/models';
-import { HttpService } from 'src/app/services/http.service';
+import { APIResponse, Game } from '../../models';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  public sort!: string;
-  public games: Array<Game> = [];
+export class HomeComponent implements OnInit, OnDestroy {
+  public sort: string;
+  public games: Array<Game>;
+  private routeSub: Subscription;
+  private gameSub: Subscription;
 
   constructor(
     private httpService: HttpService,
+    private router: Router,
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
       } else {
@@ -29,10 +33,26 @@ export class HomeComponent implements OnInit {
   }
 
   searchGames(sort: string, search?: string): void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGameList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
-        console.log(gameList);
+        console.log(gameList.results);
+        this.games = gameList.results;
       });
+  }
+
+  openGameDetails(id: number): void {
+    console.log(id);
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
   }
 }
--- src/app/models.ts
+++ src/app/models.ts
@@ -1,4 +1,5 @@
 export interface Game {
+  id: number;
   background_image: string;
   name: string;
   released: string;
@@ -24,7 +25,7 @@ interface Genre {
 
 interface ParentPlatform {
   platform: {
-    name: string;
+    slug: string;
   };
 }
 

75e36c231aedf8b455d700bb7f12335f016b9402
Ivan Petrovic <61566864+petradev79@users.noreply.github.com>
2022-09-27 10:50:26 +0200

    Create http service and interceptors and models (#3)
    * create HttpService
    
    * create models file with interfaces
    
    * create http interceptors for parameters (HttpErrorsInterceptor, HttpHeadersInterceptor)
    
    Co-authored-by: Ivan Petrovic <ivan.petrovic@ingsoftware.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -14,6 +14,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
+import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 
 @NgModule({
   declarations: [AppComponent, SearchBarComponent, HomeComponent],
@@ -29,7 +31,18 @@ import { HomeComponent } from './components/home/home.component';
     MatTabsModule,
     MatSelectModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
